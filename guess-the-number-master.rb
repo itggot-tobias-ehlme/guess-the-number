6950374ed4b2e53c686197beefad14cfa0ce867c
itggot-tobias-ehlme <tobias.ehlme@itggot.se>
2017-03-09 12:09:45 +0100

    Klar
    Fixade 2 nya nivåer,

--- guess-the-number-master.rb
+++ guess-the-number-master.rb
@@ -3,10 +3,12 @@ def s
 end
 
 def welcome
+  system('cls')
   puts "Welcome to Guess the number!"
   puts "Please choose difficulty!"
-  puts "You can only choose level 1 atm"
-  puts "::[1]:: Level one, (1-10)"
+  puts "::[1]:: Level One, (1-10)"
+  puts "::[2]:: Level Two, (1-15)"
+  puts "::[3]:: Level Three, (1-20)"
   difficulty
 end
 
@@ -24,6 +26,46 @@ def difficulty
       leveltres
     end
   end
+end
+
+def endgame
+  system('cls')
+  x = "Wake up, Neo."
+  i = 0
+  while i < x.length
+    print x[i]
+    sleep 0.1
+    i += 1
+  end
+  s
+  system('cls')
+  x = "Teh Matrx habe you."
+  i = 0
+  while i < x.length
+    print x[i]
+    sleep 0.1
+    i += 1
+  end
+  s
+  system('cls')
+  x = "Don't try to CTRL + C me boi, Follow the white rabbit."
+  i = 0
+  while i < x.length
+    print x[i]
+    sleep 0.1
+    i += 1
+  end
+  s
+  system('cls')
+  x = "Knock, knock, Neo."
+  i = 0
+  while i < x.length
+    print x[i]
+    sleep 0.1
+    i += 1
+  end
+  s
+  exit
 end
 
 def leveluno
@@ -48,39 +90,7 @@ def leveluno
       puts "You have #{tries} tries left"
     end
     if tries == 0
-      system('cls')
-      x = "Wake up, Neo."
-      i = 0
-      while i < x.length
-        print x[i]
-        sleep 0.2
-        i += 1
-      end
-      system('cls')
-      x = "Teh Matrx habe you."
-      i = 0
-      while i < x.length
-        print x[i]
-        sleep 0.2
-        i += 1
-      end
-      system('cls')
-      x = "Don't try to CTRL + C me boi, Follow the white rabbit"
-      i = 0
-      while i < x.length
-        print x[i]
-        sleep 0.2
-        i += 1
-      end
-      system('cls')
-      x = "Knock, knock, Neo."
-      i = 0
-      while i < x.length
-        print x[i]
-        sleep 0.2
-        i += 1
-      end
-      exit
+    endgame
     end
     if input == secret
       puts "Gz you have found the number!"
@@ -88,4 +98,67 @@ def leveluno
     end
   end
 end
+
+def leveldos
+  puts "You have choosen level 2!"
+  puts "Guess the secret number from 1-15"
+  puts "You have 5 tries"
+  secret = rand(1..15)
+  tries = 5
+  running = true
+  while running
+    input = gets.to_i
+    if input > secret
+      tries -= 1
+      puts "#{input} is higher than the secret number"
+      puts "Guess again!"
+      puts "You have #{tries} tries left"
+    end
+    if input < secret
+      tries -= 1
+      puts "#{input} is lower than the secret number"
+      puts "Guess again!"
+      puts "You have #{tries} tries left"
+    end
+    if tries == 0
+    endgame
+    end
+    if input == secret
+      puts "Gz you have found the number!"
+      exit
+    end
+  end
+end
+
+def leveltres
+  puts "You have choosen level 3!"
+  puts "Guess the secret number from 1-20"
+  puts "You have 5 tries"
+  secret = rand(1..20)
+  tries = 5
+  running = true
+  while running
+    input = gets.to_i
+    if input > secret
+      tries -= 1
+      puts "#{input} is higher than the secret number"
+      puts "Guess again!"
+      puts "You have #{tries} tries left"
+    end
+    if input < secret
+      tries -= 1
+      puts "#{input} is lower than the secret number"
+      puts "Guess again!"
+      puts "You have #{tries} tries left"
+    end
+    if tries == 0
+    endgame
+    end
+    if input == secret
+      puts "Gz you have found the number!"
+      exit
+    end
+  end
+end
+
 welcome
